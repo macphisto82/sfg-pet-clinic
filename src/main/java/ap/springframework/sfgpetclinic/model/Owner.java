b65476eb2c0cecd7d310bb011f72c0aeac17a1c9
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-13 11:44:41 +0100

    Adding POJOs Closes #2

---
+++ src/main/java/ap/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package ap.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/ap/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package ap.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
