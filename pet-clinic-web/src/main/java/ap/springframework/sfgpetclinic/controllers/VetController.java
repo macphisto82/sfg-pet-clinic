ca409bf7c0fd59dda00cc2e5b437a65ac5e01879
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-15 22:16:14 +0100

    Add vets list. closes #15

--- pet-clinic-web/src/main/java/ap/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ap/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,24 @@
 package ap.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ap.springframework.sfgpetclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String vetsList() {
-		
+	public String vetsList(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
