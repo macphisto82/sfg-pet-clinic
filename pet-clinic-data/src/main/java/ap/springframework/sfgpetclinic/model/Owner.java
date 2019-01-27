de997997a3c2dc20d05ce4b65fe4650211b55603
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-27 14:11:07 +0100

    Created Pet Type, Pet, and Visit Entities. closes #26

--- pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package ap.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	private static final long serialVersionUID = 4435947974885655300L;
 
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
 }
