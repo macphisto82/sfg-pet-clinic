a8e0015311c296a271b0eccd22df14576646b5f8
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-27 14:30:53 +0100

    Created PetType Map Service Impl. closes #29

---
+++ pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ap.springframework.sfgpetclinic.services;
+
+import ap.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
