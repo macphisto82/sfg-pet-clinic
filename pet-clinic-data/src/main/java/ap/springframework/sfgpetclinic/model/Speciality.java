2beba3b347f23648e59bcee7f8b5503736c3acb2
macphisto82 <alessandro.pilonato@gmail.com>
2019-02-11 16:03:07 +0100

    Converted Vets to JPA Entities. closes #37

--- pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,16 @@
 package ap.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
 	private static final long serialVersionUID = 2924551864721126385L;
 	
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
