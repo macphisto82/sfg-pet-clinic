7c3cbaced54aa24dd7ff3394d75c1c2d99108818
macphisto82 <alessandro.pilonato@gmail.com>
2019-02-12 12:34:04 +0100

    init task

--- pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ap/springframework/sfgpetclinic/model/Person.java
@@ -3,6 +3,13 @@ package ap.springframework.sfgpetclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
@@ -13,17 +20,4 @@ public class Person extends BaseEntity {
 	
 	@Column(name = "last_name")
 	private String lastName;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 }
